2e31f1560226042c3d07f56017eb5d209629ffed
unknown <luutam_viva@yahoo.com>
2024-04-02 17:24:50 +0200

    créer project

---
+++ sujet_ECF/server/search-sugesstion-service/src/test/java/org/example/searchsugesstionservice/SearchSugesstionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.searchsugesstionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSugesstionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
